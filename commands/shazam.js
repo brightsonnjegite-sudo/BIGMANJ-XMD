78ddafb25e86bcf9915d3660ed0d38f87af9fa41
BIGMANj <brightsonnjegite@gmail.com>
2026-05-29 18:59:16 +0300

    Update shazam.js

--- commands/shazam.js
+++ commands/shazam.js
@@ -92,7 +92,7 @@ async function shazamCommand(sock, chatId, message) {
             await sendButtons(sock, chatId, {
                 title: '🎧 SONG FINDER',
                 text: caption,
-                footer: 'BIGMANj•DATshazambot',
+                footer: 'BIGMANj•DATshazam bot',
                 buttons: [
                     { id: playCmd, text: '📥 ' + artist + ' - ' + title }
                 ]
