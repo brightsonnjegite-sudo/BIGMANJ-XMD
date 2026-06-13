42e8cf49e52d2f84a2c6daa3ec50ca39fe847d7e
BIGMANj <brightsonnjegite@gmail.com>
2026-06-13 08:43:36 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,6 +1,7 @@
 async function unmuteCommand(sock, chatId) {
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: 'The group has been unmuted.' });
+    const responseText = `✅ The group has been unmuted.\n\n© bigmanj tech ™ with ♥︎`;
+    await sock.sendMessage(chatId, { text: responseText });
 }
 
 module.exports = unmuteCommand;
